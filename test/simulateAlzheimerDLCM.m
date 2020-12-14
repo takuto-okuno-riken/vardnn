723d30448d9e89981e8a3000ac29b02e45986125
takuto-okuno-t5 <okuno.takuto.t5@dc.tohoku.ac.jp>
2020-12-14 10:54:40 +0900

    fixed alzheimer simulation type10.

--- test/simulateAlzheimerDLCM.m
+++ test/simulateAlzheimerDLCM.m
@@ -175,9 +175,9 @@ b(:,3) = squeeze(adZij(i,j,:));
     [vad12DLWs, meanVad12DLWns, stdVad12DLWns] = retrainDLCMAndEC(vad11DLWnss, cnS11, cnIS11, roiNames, 'vad12ns');
     [vad12bDLWs, vad12DLWnss, ~, ~] = calculateNodeSignals(cnSignals, cnS11, cnIS11, roiNames, 'vad12ns', 'dlw', 'adsim');
 
-    [~,~,~] = calculateAlzWilcoxonTest(vad11DLWnss, vad12DLWnss, roiNames, 'vad11ns', 'vad12ns', 'dlw', 1, 'ranksum');
-    [~,~,~] = calculateAlzWilcoxonTest(vad9DLWs, vad12bDLWs, roiNames, 'vad9ec', 'vad12ec', 'dlw', 1, 'ranksum');
-    [~,~,~] = calculateAlzWilcoxonTest(vad9DLWs, vad12bDLWs, roiNames, 'vad9ec', 'vad12ec', 'dlw', 1, 'ttest2');
+%    [~,~,~] = calculateAlzWilcoxonTest(vad11DLWnss, vad12DLWnss, roiNames, 'vad11ns', 'vad12ns', 'dlw', 1, 'ranksum');
+%    [~,~,~] = calculateAlzWilcoxonTest(vad9DLWs, vad12bDLWs, roiNames, 'vad9ec', 'vad12ec', 'dlw', 1, 'ranksum');
+%    [~,~,~] = calculateAlzWilcoxonTest(vad9DLWs, vad12bDLWs, roiNames, 'vad9ec', 'vad12ec', 'dlw', 1, 'ttest2');
 
     % --------------------------------------------------------------------------------------------------------------
     % re-training DLCM network (type 10 : EC, net) (optimise for DLCM training)
@@ -187,7 +187,7 @@ b(:,3) = squeeze(adZij(i,j,:));
 
     vad13DLWnss(:,2:ROINUM+1,:) = vad13Zij;
     vad13DLWs = abs(vad13Zi - vad13Zij);
-    vad13DLWnss(:,1,:) = vadDLWnss(:,1,:) + nanmean(vad13DLWs,1) * 0.5;
+    vad13DLWnss(:,1,:) = vadDLWnss(:,1,:) + nanmean(vad13DLWs,2) * 0.5;
 
     [vad14DLWs, meanVad14DLWns, stdVad14DLWns] = retrainDLCMAndEC(vad13DLWnss, cnS2, cnIS2, roiNames, 'vad12ns');
     [vad14bDLWs, vad14DLWnss, ~, ~] = calculateNodeSignals(cnSignals, cnS2, cnIS2, roiNames, 'vad12ns', 'dlw', 'adsim');
