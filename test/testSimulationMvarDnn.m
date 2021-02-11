d0866ab2c5a33c734ff23d59112d7a163739dc78
takuto-okuno-t5 <okuno.takuto.t5@dc.tohoku.ac.jp>
2021-02-12 01:22:35 +0900

    fixed sim mvar dnn.

--- test/testSimulationMvarDnn.m
+++ test/testSimulationMvarDnn.m
@@ -81,12 +81,24 @@ function testSimulationMVAR
         [time, loss, rsme] = getDlcmTrainingResult(net);
         disp(['train result time=' num2str(time) ', loss=' num2str(loss) ', rsme=' num2str(rsme)]);
 
-        save(mvardnnFile, 'net');
+        % init mVAR DNN network (linear)
+        net2 = initDlcmNetwork(si, exSignal, [], exControl, lags, []);
+        % training mVAR DNN network (linear)
+        net2 = trainDlcmNetwork(si, exSignal, [], exControl, net2, options);
+        [time, loss, rsme] = getDlcmTrainingResult(net2);
+        disp(['train result time=' num2str(time) ', loss=' num2str(loss) ', rsme=' num2str(rsme)]);
+
+        save(mvardnnFile, 'net', 'net2');
     end
 
     % simulate mVAR DNN (DLCM) network with 1st frame & exogenous input signal
     [S, time] = simulateMvarDnnNetwork(si, exSignal, [], exControl, net);
     figure; [mae, maeerr] = plotTwoSignals(si, S);
+    disp(['simulation time=' num2str(time) ', mae=' num2str(mae)]);
+
+    % simulate mVAR DNN (DLCM) linear network with 1st frame & exogenous input signal
+    [S2, time] = simulateMvarDnnNetwork(si, exSignal, [], exControl, net2);
+    figure; [mae, maeerr] = plotTwoSignals(si, S2);
     disp(['simulation time=' num2str(time) ', mae=' num2str(mae)]);
 
     % simulate mVAR network with 1st frame & exogenous input signal
@@ -97,10 +109,12 @@ function testSimulationMVAR
     % show original & simulated signal FC
     figure; FC = plotFunctionalConnectivity(si);
     figure; FC = plotFunctionalConnectivity(S);
+    figure; FC = plotFunctionalConnectivity(S2);
     figure; FC = plotFunctionalConnectivity(S3);
     % show original & simulated signal granger causality index (gc-EC)
     figure; gcI = plotPairwiseGCI(si);
     figure; gcI = plotPairwiseGCI(S);
+    figure; gcI = plotPairwiseGCI(S2);
     figure; gcI = plotPairwiseGCI(S3);
 end
 
