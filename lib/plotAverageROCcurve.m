294deb19df8ad9981e7e42f8402f8ab676fe51d1
takuto-okuno-riken <66657250+takuto-okuno-riken@users.noreply.github.com>
2021-04-20 14:11:54 +0900

    fixed multi length ROC curves.

--- lib/plotAverageROCcurve.m
+++ lib/plotAverageROCcurve.m
@@ -1,10 +1,18 @@
 
 function [x, y] = plotAverageROCcurve(roc, N, line, col, width)
-    x = zeros(N,size(roc{1,1},2));
-    y = zeros(N,size(roc{1,1},2));
+    maxCol = 0;
     for k=1:N
-        x(k,:) = roc{k,1};
-        y(k,:) = roc{k,2};
+        if size(roc{k,1},2)>maxCol, maxCol=size(roc{k,1},2); end
+    end
+    x = nan(N,maxCol);
+    y = nan(N,maxCol);
+    for k=1:N
+        len = size(roc{k,1},2);
+        if len==0, continue; end
+        x(k,:) = roc{k,1}(end);
+        y(k,:) = roc{k,2}(end);
+        x(k,1:len) = roc{k,1}(1:len);
+        y(k,1:len) = roc{k,2}(1:len);
     end
     mx = mean(x,1);
     my = mean(y,1);
