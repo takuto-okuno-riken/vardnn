f4c4f7219380d0de3aa4311778c92bc2f717a7e6
takuto-okuno-t5 <okuno.takuto.t5@dc.tohoku.ac.jp>
2021-07-07 12:39:04 +0900

    fixed plot avg roc curve.

--- lib/plotAverageROCcurve.m
+++ lib/plotAverageROCcurve.m
@@ -1,21 +1,40 @@
 
-function [x, y] = plotAverageROCcurve(roc, N, line, col, width)
-    maxCol = 0;
+function [mx, my] = plotAverageROCcurve(roc, N, line, col, width)
+    step = 100;
+    mx = 0:step;
+    y = nan(N,length(mx));
     for k=1:N
-        if size(roc{k,1},2)>maxCol, maxCol=size(roc{k,1},2); end
+        tX = roc{k,1};
+        tY = roc{k,2};
+        lasti = 1;
+        for i = 1:length(tX)
+            if i < length(tX) && tX(i) == tX(i+1), continue; end
+            
+            % find mx;
+            mx1=floor(tX(lasti)*step);
+            mx2=floor(tX(i)*step);
+            idx1=find(mx==mx1);
+            idx2=find(mx==mx2);
+            % linear interpolation
+            if tY(lasti) == tY(i) || idx1 == idx2
+                y(k,idx1:idx2) = tY(i);
+            else
+                dy = tY(i)-tY(lasti);
+                for j=idx1:idx2
+                    y(k,j) = dy*((j-idx1)/(idx2-idx1)) + tY(lasti);
+                end
+            end
+            lasti = i;
+        end
+        for j=2:length(mx)
+            if isnan(y(k,j)), y(k,j)=y(k,j-1); end
+        end
+        y(k,end) = 1;
+%        hold on; plot(mx, y(k,:)); hold off;
     end
-    x = nan(N,maxCol);
-    y = nan(N,maxCol);
-    for k=1:N
-        len = size(roc{k,1},2);
-        if len==0, continue; end
-        x(k,:) = roc{k,1}(end);
-        y(k,:) = roc{k,2}(end);
-        x(k,1:len) = roc{k,1}(1:len);
-        y(k,1:len) = roc{k,2}(1:len);
-    end
-    mx = mean(x,1);
-    my = mean(y,1);
+    mx = mx / step;
+    mx = [0, mx, 1];
+    my = [0, mean(y,1), 1];
     % plot line
     plot(mx, my, line,'Color',col,'LineWidth',width);
 end
--- lib/plotErrorROCcurve.m
+++ lib/plotErrorROCcurve.m
@@ -1,13 +1,40 @@
 
-function [x, y] = plotErrorROCcurve(roc, N, col)
-    x = zeros(N,size(roc{1,1},2));
-    y = zeros(N,size(roc{1,1},2));
+function [mx, my] = plotErrorROCcurve(roc, N, col)
+    step = 100;
+    mx = 0:step;
+    y = nan(N,length(mx));
     for k=1:N
-        x(k,:) = roc{k,1};
-        y(k,:) = roc{k,2};
+        tX = roc{k,1};
+        tY = roc{k,2};
+        lasti = 1;
+        for i = 1:length(tX)
+            if i < length(tX) && tX(i) == tX(i+1), continue; end
+            
+            % find mx;
+            mx1=floor(tX(lasti)*step);
+            mx2=floor(tX(i)*step);
+            idx1=find(mx==mx1);
+            idx2=find(mx==mx2);
+            % linear interpolation
+            if tY(lasti) == tY(i) || idx1 == idx2
+                y(k,idx1:idx2) = tY(i);
+            else
+                dy = tY(i)-tY(lasti);
+                for j=idx1:idx2
+                    y(k,j) = dy*((j-idx1)/(idx2-idx1)) + tY(lasti);
+                end
+            end
+            lasti = i;
+        end
+        for j=2:length(mx)
+            if isnan(y(k,j)), y(k,j)=y(k,j-1); end
+        end
+        y(k,end) = 1;
+%        hold on; plot(mx, y(k,:)); hold off;
     end
-    mx = mean(x,1);
-    my = mean(y,1);
+    mx = mx / step;
+    mx = [0, mx, 1];
+    my = [0, mean(y,1), 1];
     % check y=1,x=0 (no error)
     for i=1:length(mx)
         if mx(i)==0 && my(i)==1
@@ -15,12 +42,11 @@ function [x, y] = plotErrorROCcurve(roc, N, col)
         end
     end
     % error area
-    errx = std(x,1,1) / sqrt(size(x,1));
     erry = std(y,1,1) / sqrt(size(y,1));
-    xt = mx -errx;
-    yt = my +erry;
-    xb = mx +errx;
-    yb = my -erry;
+    xt = mx;
+    yt = my +[0 erry 0];
+    xb = mx;
+    yb = my -[0 erry 0];
     Y = [yb fliplr(yt)];
     X = [xb fliplr(xt)];
     patch(X,Y,col,'EdgeColor','none','FaceAlpha',.04);
