5109afe17af181fb9f0f40a65767f46f0ca3b85f
takuto-okuno-riken <66657250+takuto-okuno-riken@users.noreply.github.com>
2021-08-14 14:51:55 +0900

    fixed calc connectivity for test.

---
+++ test/plotConnectomeMatrix.m
@@ -0,0 +1,104 @@
+% this function is only for ADNI2 alzheimer analysis
+
+function plotConnectomeMatrix(meanWeights, algorithm, group, lags)
+    switch(algorithm)
+    case 'fc'
+        clims = [-1,1];
+        titleStr = [group ' : Functional Connectivity'];
+        sigWeights = meanWeights;
+    case 'fca'
+        clims = [0,1];
+        titleStr = [group ' : Functional Connectivity (Abs)'];
+        sigWeights = meanWeights;
+    case 'tsfc'
+        sigma = std(meanWeights(:),1,'omitnan');
+        avg = mean(meanWeights(:),'omitnan');
+        sigWeights = (meanWeights - avg) / sigma;
+        clims = [-3, 3];
+        titleStr = [group ' : Time shifted Functional Connectivity (' num2str(lags) ')'];
+    case 'tsfca'
+        sigma = std(meanWeights(:),1,'omitnan');
+        avg = mean(meanWeights(:),'omitnan');
+        sigWeights = (meanWeights - avg) / sigma;
+        clims = [-3, 3];
+        titleStr = [group ' : Time shifted Functional Connectivity (Abs) (' num2str(lags) ')'];
+    case {'pc','pcpc','lsopc','plspc'}
+        clims = [-1,1];
+        titleStr = [group ' : ' algorithm];
+        sigWeights = meanWeights;
+    case 'wcs'
+        clims = [-1,1];
+        titleStr = [group ' : Wavelet Coherence Cross Spectrum'];
+        sigWeights = meanWeights;
+    case 'gc'
+        sigma = std(meanWeights(:),1,'omitnan');
+        avg = mean(meanWeights(:),'omitnan');
+        sigWeights = (meanWeights - avg) / sigma;
+        clims = [-3, 3];
+        titleStr = [group ' : multivariate Granger Causality(' num2str(lags) ') Index'];
+    case 'pgc'
+        sigma = std(meanWeights(:),1,'omitnan');
+        avg = mean(meanWeights(:),'omitnan');
+        sigWeights = (meanWeights - avg) / sigma;
+        clims = [-3, 3];
+        titleStr = [group ' : pairwise Granger Causality(' num2str(lags) ') Index'];
+    case 'te'
+        sigma = std(meanWeights(:),1,'omitnan');
+        avg = mean(meanWeights(:),'omitnan');
+        sigWeights = (meanWeights - avg) / sigma;
+        clims = [-3, 3];
+        titleStr = [group ' : Transfer Entropy (LINER) (' num2str(lags) ')'];
+    case 'pcs'
+        clims = [-1,1];
+        titleStr = [group ' : PC-stable-max'];
+        sigWeights = meanWeights;
+    case 'cpc'
+        clims = [-1,1];
+        titleStr = [group ' : Conservative PC'];
+        sigWeights = meanWeights;
+    case 'fges'
+        sigma = std(meanWeights(:),1,'omitnan');
+        avg = mean(meanWeights(:),'omitnan');
+        sigWeights = (meanWeights - avg) / sigma;
+        clims = [-3, 3];
+        titleStr = [group ' : FGES'];
+    case 'dlg'
+        clims = [-1,1];
+        titleStr = [group ' : Direct LiNGAM'];
+        sigWeights = meanWeights;
+    case {'dlcm','dlcmB','dlcmrc'}
+        sigma = std(meanWeights(:),1,'omitnan');
+        avg = mean(meanWeights(:),'omitnan');
+        sigWeights = (meanWeights - avg) / sigma;
+        clims = [-3, 3];
+        titleStr = [group ' : VARDNN(' num2str(lags) ') Granger Causality Index'];
+    case {'dlw','dlwB','dlwrc'}
+        sigma = std(meanWeights(:),1,'omitnan');
+        avg = mean(meanWeights(:),'omitnan');
+        sigWeights = (meanWeights - avg) / sigma;
+        clims = [-3, 3];
+        titleStr = [group ' : VARDNN(' num2str(lags) ') DI'];
+    case {'mvarec','pvarec', 'mpcvarec','ppcvarec', 'mplsvarec','pplsvarec', 'mlsovarec','plsovarec','pcdlw'}
+        sigma = std(meanWeights(:),1,'omitnan');
+        avg = mean(meanWeights(:),'omitnan');
+        sigWeights = (meanWeights - avg) / sigma;
+        clims = [-3, 3];
+        titleStr = [group ' : ' algorithm '(' num2str(lags) ') Index'];
+    case {'mvar','pvar' 'mpcvar','ppcvar', 'mplsvar','pplsvar', 'mlsovar','plsovar'}
+        sigma = std(meanWeights(:),1,'omitnan');
+        avg = mean(meanWeights(:),'omitnan');
+        sigWeights = (meanWeights - avg) / sigma;
+        clims = [-3, 3];
+        titleStr = [group ' : ' algorithm '(' num2str(lags) ') Index'];
+    case {'mpcvargc','ppcvargc', 'mplsvargc','pplsvargc', 'mlsovargc','plsovargc', 'pcgc','pcdl'}
+        sigma = std(meanWeights(:),1,'omitnan');
+        avg = mean(meanWeights(:),'omitnan');
+        sigWeights = (meanWeights - avg) / sigma;
+        clims = [-3, 3];
+        titleStr = [group ' : ' algorithm '(' num2str(lags) ')-GC Index'];
+    end
+    imagesc(sigWeights,clims);
+    daspect([1 1 1]);
+    title(titleStr);
+    colorbar;
+end
